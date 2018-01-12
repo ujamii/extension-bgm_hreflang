8d8b8b8f1c1c4d3626e61079b68148278c80a719
Marco Huber <marco.huber@bgm-gmbh.de>
2018-01-12 15:32:35 +0100

    [TASK] Automated tests: add dependencies for testing

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,11 +32,19 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '3.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.6.99',
+			'typo3' => '7.6.0-7.6.99',
 		),
-		'conflicts' => array(
+		'conflicts' => array(),
+		'suggests' => array(),
+	),
+	'autoload' => array(
+		'psr-4' => array(
+			'BGM\\BgmHreflang\\' => 'Classes/',
 		),
-		'suggests' => array(
+	),
+	'autoload-dev' => array(
+		'psr-4' => array(
+			'BGM\\BgmHreflang\\Tests\\' => 'Tests/',
 		),
 	),
 );
