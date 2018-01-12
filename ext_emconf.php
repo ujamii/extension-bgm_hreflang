0da69bbf014cad70ee894f10ad5da63c15252cd5
Marco Huber <marco.huber@bgm-gmbh.de>
2018-01-12 15:06:18 +0100

    [TASK] Automated tests: reduce dependency to write tests for the old version

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '3.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '8.7.0-8.7.99',
+			'typo3' => '6.2.0-7.6.99',
 		),
 		'conflicts' => array(
 		),
