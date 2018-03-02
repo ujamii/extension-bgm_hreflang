7961b292d1c519a1240fa7b6d6821acd051cfd10
Marco Huber <marco.huber@bgm-gmbh.de>
2018-03-02 16:23:19 +0100

    [BREAKING] Drop TYPO3 7.6 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '3.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.0-8.7.99',
+			'typo3' => '8.0.0-8.7.99',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
